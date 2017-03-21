6dd73f655110c8cbb680883cc062b022f339b9b4
Holly <hmowilliams@gmail.com>
2017-03-21 19:49:20 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ var warningRE = new RegExp("suicide");
 var testWarningString = "Does this sentence have the word suicide in it?";
 
 if ( warningRE.test(testWarningString)) {
-  alert("If this is an emergency, please call 911 or the National Suicide Prevention Lifeline at 1-800-273-8255.
-        \n You can also visit their website at\: http\:\/\/suicidepreventionlifeline.org\/");
+  alert("If this is an emergency, please call 911 or the National Suicide Prevention Lifeline at 1-800-273-8255." 
+        + "\n You can also visit their website at\: http\:\/\/suicidepreventionlifeline.org\/");
 }
