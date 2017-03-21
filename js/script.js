e05b4aafe5f198d89b5d8ada91765fe803bc2dfa
Holly <hmowilliams@gmail.com>
2017-03-21 19:51:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var name = prompt("What is your name?");
 alert("Hello, " + name);
 
-var warningRE = new RegExp("suicide");
+var warningRE = /suicide/;
 
 var testWarningString = "Does this sentence have the word suicide in it?";
 
