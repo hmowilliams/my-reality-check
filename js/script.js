18fb7de92eb125e5d82512f7340a31fbeb56e38c
Holly <hmowilliams@gmail.com>
2017-03-21 20:48:40 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,6 +40,6 @@ switch (journalLength) {
     break;
 }
 
-var headline = document.getElementById("h1");
+var headline = document.getElementById("title");
 
 headline.innerHTML = "Wow, a new headline!";
